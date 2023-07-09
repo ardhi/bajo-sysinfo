c74f52baf267a4112bd5579de1328ee3e42fa367
Ardhi Lukianto <ardhi@lukianto.com>
2023-07-10 06:16:24 +0700

    Updates

---
+++ bajoCli/tool.js
@@ -0,0 +1,46 @@
+import si from 'systeminformation'
+import toolBajo from './tool/bajo.js'
+
+const withoutTypes = ['version', 'observe', 'get', 'getAllData', 'getDynamicData', 'getStaticData',
+  'dockerAll']
+const withParams = ['processLoad', 'services', 'inetChecksite', 'inetLatency']
+const secondCall = ['fsStats', 'disksIO', 'networkStats', 'currentLoad']
+const extTypes = ['bajoApp', 'bajoPlugin']
+
+async function tool ({ path, args = [] }) {
+  const { getConfig, print, importPackage, saveAsDownload } = this.bajo.helper
+  const { prettyPrint } = this.bajoCli.helper
+  const [_, delay, stripAnsi] = await importPackage('lodash::bajo',
+    'delay::bajo', 'strip-ansi::bajo-cli')
+  const { select } = await importPackage('@inquirer/prompts::bajo-cli')
+  const paths = _.concat(_.without(_.keys(si), ...withoutTypes), extTypes).sort()
+  const choices = _.map(paths, c => {
+    return { value: c }
+  })
+  const config = getConfig()
+  if (!path) {
+    path = await select({
+      message: `Please select a method:`,
+      pageSize: 10,
+      choices
+    })
+  }
+  if (!paths.includes(path)) print.fatal(`Unsupported methods '${path}'`)
+  const spinner = print.ora('Retrieving...').start()
+  const handler = path.startsWith('bajo') ? toolBajo[path].bind(this) : si[path]
+  if (!path.startsWith('bajo') && withParams.includes(path)) args[0] = args[0] || '*'
+  if (secondCall.includes(path)) {
+    await handler(...args)
+    await delay(3000)
+  }
+  let result = await handler(...args)
+  spinner.info('Done!')
+  result = config.pretty ? (await prettyPrint(result)) : JSON.stringify(result, null, 2)
+  if (config.save) {
+    const file = `/${path}.${config.pretty ? 'txt' : 'json'}`
+    const fullPath = await saveAsDownload(file, stripAnsi(result), 'bajoSysinfo')
+    print.ora(`Saved as '${fullPath}'`, true).succeed()
+  } else console.log(result)
+}
+
+export default tool
---
+++ bajoCli/tool/bajo.js
@@ -0,0 +1,18 @@
+async function bajoApp () {
+
+}
+
+async function bajoPlugin () {
+  const { importPackage } = this.bajo.helper
+  const _ = await importPackage('lodash::bajo')
+  const keys = _.without(_.keys(this), 'bajo', 'app')
+  const result = _.map(keys, k => {
+    const def = { name: k, package: this[k].config.pkg.name }
+    const item = _.merge({}, def, _.omit(this[k].config.pkg, ['name']))
+    item.helper = _.keys(this[k].helper)
+    return item
+  })
+  return result
+}
+
+export default { bajoApp, bajoPlugin }
