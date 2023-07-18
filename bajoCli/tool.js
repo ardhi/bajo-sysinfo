f38c7393863e663d90cc22866df98dca9d805aa6
Ardhi Lukianto <ardhi@lukianto.com>
2023-07-18 19:10:47 +0700

    StandardJs

--- bajoCli/tool.js
+++ bajoCli/tool.js
@@ -10,8 +10,8 @@ const extTypes = ['bajoApp', 'bajoPlugin']
 async function tool ({ path, args = [] }) {
   const { getConfig, print, importPkg, saveAsDownload } = this.bajo.helper
   const { prettyPrint } = this.bajoCli.helper
-  const { concat, without, keys, map } = await importPkg('lodash-es::bajo')
-  const [delay, stripAnsi, select] = await importPkg('delay::bajo', 'strip-ansi::bajo-cli', '@inquirer/select::bajo-cli')
+  const { concat, without, keys, map } = await importPkg('lodash-es')
+  const [delay, stripAnsi, select] = await importPkg('delay', 'bajo-cli:strip-ansi', 'bajo-cli:@inquirer/select')
   const paths = concat(without(keys(si), ...withoutTypes), extTypes).sort()
   const choices = map(paths, c => {
     return { value: c }
@@ -19,15 +19,18 @@ async function tool ({ path, args = [] }) {
   const config = getConfig()
   if (!path) {
     path = await select({
-      message: print.__(`Please select a method:`),
+      message: print.__('Please select a method:'),
       pageSize: 10,
       choices
     })
   }
-  if (!paths.includes(path)) print.fatal(`Unknown method '%s'`, path)
+  if (!paths.includes(path)) print.fatal('Unknown method \'%s\'', path)
   const spinner = print.bora('Retrieving...').start()
   const handler = path.startsWith('bajo') ? toolBajo[path].bind(this) : si[path]
-  if (!path.startsWith('bajo') && withParams.includes(path)) args[0] = args[0] || '*'
+  if (!path.startsWith('bajo')) {
+    if (withParams.includes(path)) args[0] = args[0] || '*'
+    else args = []
+  }
   if (secondCall.includes(path)) {
     await handler(...args)
     await delay(3000)
@@ -37,8 +40,7 @@ async function tool ({ path, args = [] }) {
   result = config.pretty ? (await prettyPrint(result)) : JSON.stringify(result, null, 2)
   if (config.save) {
     const file = `/${path}.${config.pretty ? 'txt' : 'json'}`
-    const fullPath = await saveAsDownload(file, stripAnsi(result), 'bajoSysinfo')
-    print.bora(`Saved as '%s'`, fullPath, { skipSilence: true }).succeed()
+    await saveAsDownload(file, stripAnsi(result), 'bajoSysinfo')
   } else console.log(result)
 }
 
--- bajoCli/tool/bajo.js
+++ bajoCli/tool/bajo.js
@@ -4,7 +4,7 @@ async function bajoApp () {
 
 async function bajoPlugin () {
   const { importPkg } = this.bajo.helper
-  const { without, map, keys, merge, omit } = await importPkg('lodash-es::bajo')
+  const { without, map, keys, merge, omit } = await importPkg('lodash-es')
   const ky = without(keys(this), 'bajo', 'app')
   const result = map(ky, k => {
     const def = { name: k, package: this[k].config.pkg.name }
