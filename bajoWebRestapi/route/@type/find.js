4b1b63e11eef1dbb9d81eefa6c49465cdf66ea86
Ardhi Lukianto <ardhi@lukianto.com>
2023-11-15 10:19:45 +0700

    Updates

--- bajoWebRestapi/route/@type/find.js
+++ bajoWebRestapi/route/@type/find.js
@@ -1,11 +1,11 @@
 async function find (ctx, req, reply) {
   const { paginate, importPkg } = this.bajo.helper
   const { prepPagination } = this.bajoDb.helper
-  const { getFilter, transformResult } = this.bajoWebRestapi.helper
+  const { parseFilter, transformResult } = this.bajoWebRestapi.helper
   const { getInfo } = this.bajoSysinfo.helper
   const { isArray } = await importPkg('lodash-es')
   const item = await getInfo(req.params.type)
-  const filter = await getFilter(req)
+  const filter = parseFilter(req)
   if (!isArray(item)) {
     const data = { data: item, success: true, statusCode: 200 }
     return await transformResult({ data, req, reply })
--- bajoWebRestapi/route/types/find.js
+++ bajoWebRestapi/route/types/find.js
@@ -1,9 +1,9 @@
 async function find (ctx, req, reply) {
   const { paginate } = this.bajo.helper
   const { prepPagination } = this.bajoDb.helper
-  const { getFilter, transformResult } = this.bajoWebRestapi.helper
+  const { parseFilter, transformResult } = this.bajoWebRestapi.helper
   const { getTypes } = this.bajoSysinfo.helper
-  const filter = await getFilter(req)
+  const filter = parseFilter(req)
   const all = await getTypes()
   const data = paginate(all, await prepPagination(filter))
   return await transformResult({ data, req, reply, options: { forFind: true } })
