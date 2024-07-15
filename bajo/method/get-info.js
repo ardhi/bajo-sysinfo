3566cbbd28d82c51f9934a9c758ad190f59f8f7d
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-15 21:19:17 +0700

    Refactor: new error handling

--- bajo/method/get-info.js
+++ bajo/method/get-info.js
@@ -5,11 +5,11 @@ const withParams = ['processLoad', 'services', 'inetChecksite', 'inetLatency']
 const secondCall = ['fsStats', 'disksIO', 'networkStats', 'currentLoad']
 
 async function getInfo (type, args) {
-  const { importPkg, error } = this.app.bajo
+  const { importPkg } = this.app.bajo
   const delay = await importPkg('delay')
   const { map } = this.app.bajo.lib._
   const types = map(this.getTypes(), 'id')
-  if (!types.includes(type)) throw error('Unsupported type \'%s\'', type)
+  if (!types.includes(type)) throw this.error('Unsupported type \'%s\'', type)
   const handler = type.startsWith('bajo') ? toolBajo[type].bind(this) : si[type]
   if (!type.startsWith('bajo')) {
     if (withParams.includes(type)) args[0] = args[0] ?? '*'
