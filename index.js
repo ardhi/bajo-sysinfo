739124d85409acccccb3af0ff5a090bfb1417297
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:32:22 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class BajoSysinfo extends this.lib.BajoPlugin {
+  return class BajoSysinfo extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'si'
